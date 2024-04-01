82519708330c9073205f0626f3551f1fd4a0349f
seokho7 <rkwhr3256@naver.com>
2024-04-01 20:08:58 +0900

    docs: chapter-11-프로토콜-버퍼.md 추가
    js로 만드는거 분명 있을거 같은데 미리 규격 잡아놓고 계속 돌려쓰기(3버전으로)

--- test.spec.ts
+++ test.spec.ts
@@ -1,5 +1,38 @@
+class ShuffleBag {
+    private items;
+    private originalItems;
+    constructor(items) {
+        this.originalItems = [...items];
+        this.items = [...items];
+        this.shuffle();
+    }
+
+    shuffle() {
+        for (let i = this.items.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [this.items[i], this.items[j]] = [this.items[j], this.items[i]];
+        }
+    }
+
+    draw() {
+        if (this.items.length === 0) {
+            this.items = [...this.originalItems];
+            this.shuffle();
+        }
+        return this.items.pop();
+    }
+}
+
 describe('냐옹', () => {
     it('멍멍', () => {
-        expect((1 + 2)).toBe(3);
+        // 셔플 백에 넣을 아이템 예시
+        const startBox = ['A', 'B', 'C', 'D', 'E'];
+
+        const shuffleBag = new ShuffleBag(startBox);
+
+        console.log(shuffleBag.draw());
+        console.log(shuffleBag.draw());
+        console.log(shuffleBag.draw());
+        console.log(shuffleBag.draw());
     });
 });
